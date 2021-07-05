10529d16eb889f7c7b3e99608d861f1b558bd390
Baranov_Vladimir <baranov.vladimir1990@hotmail.com>
2021-07-05 22:24:46 +0400

    Homework 1, Java 2

---
+++ src/java2_Homework_1/Cat.java
@@ -0,0 +1,30 @@
+package java2_Homework_1;
+
+public class Cat implements Participants {
+
+    private String name;
+    private double maxDistance;
+    private double maxHeight;
+
+    public Cat(String name, double maxDistance, double maxHeight) {
+        this.name = name;
+        this.maxDistance = maxDistance;
+        this.maxHeight = maxHeight;
+    }
+
+    public double getDistance() {
+        return maxDistance;
+    }
+
+    public double getHeight() {
+        return maxHeight;
+    }
+
+    public void run(){
+        System.out.println(name + " бежит");
+    }
+
+    public void jump(){
+        System.out.println(name + " прыгает");
+    }
+}
