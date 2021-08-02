173af9936e987cc50866b5109bf1f7cd0055ecc0
Baranov_Vladimir <baranov.vladimir1990@hotmail.com>
2021-08-02 19:53:15 +0400

    Homework 7 Java 2

---
+++ src/java2_Homework_7/server/Server.java
@@ -0,0 +1,61 @@
+package java2_Homework_7.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+    private final List<ClientHandler> loggedUsers;
+    private final AuthService authService;
+
+    public Server() {
+        authService = new AuthService();
+        loggedUsers = new ArrayList<>();
+
+        try {
+            serverSocket = new ServerSocket(8888);
+
+            while (true){
+                Socket socket = serverSocket.accept();
+                new ClientHandler(this, socket);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    public void subscribe(ClientHandler user){
+        loggedUsers.add(user);
+
+    }
+
+    public void unsubscribe(ClientHandler user){
+        loggedUsers.remove(user);
+    }
+
+    public boolean isNotUserOccupied(String name){
+        return !isUserOccupied(name);
+    }
+
+    public boolean isUserOccupied(String name){
+        return loggedUsers.stream()
+                .anyMatch(u -> u.getName().equals(name));
+    }
+
+    public void broadcastMessage(String outboundMessage){
+
+        loggedUsers.forEach(clientHandler -> clientHandler.sendMessage(outboundMessage));
+
+    }
+
+}
