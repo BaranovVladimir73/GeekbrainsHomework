c4dc7f17d47ba5b5be2c253117af0158818c153a
Baranov_Vladimir <baranov.vladimir1990@hotmail.com>
2022-01-16 15:03:48 +0400

    EE Java 1st lesson

--- src/main/java/ru/gb/servlet/ProductServlet.java
+++ src/main/java/ru/gb/servlet/ProductServlet.java
@@ -13,26 +13,24 @@ import java.util.List;
 
 @WebServlet(name = "ProductHttpServlet", urlPatterns = {"/product"})
 public class ProductServlet extends HttpServlet {
-    public ProductServlet() {
-    }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("product", this.getProductList());
+        req.setAttribute("product", getProductList());
         this.getServletContext().getRequestDispatcher("/product.jsp").forward(req, resp);
     }
 
     private List<Product> getProductList() {
         List<Product> productList = new ArrayList();
-        productList.add(new Product(1, "bread", 40.99D));
-        productList.add(new Product(2, "fish", 240.99D));
-        productList.add(new Product(3, "Vodka", 340.99D));
-        productList.add(new Product(4, "yogurt with pineapple", 55.99D));
-        productList.add(new Product(5, "Kefir", 47.99D));
-        productList.add(new Product(6, "Indian Tea", 140.99D));
-        productList.add(new Product(7, "Sunflower oil", 120.99D));
-        productList.add(new Product(8, "Salt", 10.99D));
-        productList.add(new Product(9, "Sugar", 100.99D));
-        productList.add(new Product(10, "Kwas", 99.99D));
+        productList.add(new Product(1, "bread", 40.99));
+        productList.add(new Product(2, "fish", 240.99));
+        productList.add(new Product(3, "Vodka", 340.99));
+        productList.add(new Product(4, "yogurt with pineapple", 55.99));
+        productList.add(new Product(5, "Kefir", 47.99));
+        productList.add(new Product(6, "Indian Tea", 140.99));
+        productList.add(new Product(7, "Sunflower oil", 120.99));
+        productList.add(new Product(8, "Salt", 10.99));
+        productList.add(new Product(9, "Sugar", 100.99));
+        productList.add(new Product(10, "Kwas", 99.99));
         return productList;
     }
 }
